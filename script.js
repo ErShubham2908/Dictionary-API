76728d47d28ee30966c4a30b12b906dce5403455
ErShubham2908 <shubhamprepbytes007@gmail.com>
2023-03-06 16:07:12 +0530

    almost done

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let btn = document.getElementById('search');
 let words = document.getElementById('get_word');
 let phonetic = document.getElementById('phonetic');
 let audio = document.getElementById('audio');
-// let sound = document.getElementById('audioSource');
 let item_partOfSpeech = document.getElementById('item_partOfSpeech');
+let flag = true;
 
 btn.addEventListener('click', () => {
 	let displayErr = document.getElementById('display');
@@ -18,9 +18,9 @@ btn.addEventListener('click', () => {
 		item.map((data) => {
 			words.innerText =  `${data.word}`;
 			if(data.phonetic === undefined){
-				phonetic.style.display = 'none';
-			}
-			else{
+					phonetic.style.display = 'none';
+				}
+				else{
 				phonetic.innerText = data.phonetic;
 			}
 			item_partOfSpeech.innerText = data.meanings[0].partOfSpeech;
@@ -29,7 +29,6 @@ btn.addEventListener('click', () => {
 	})
 	.catch((err) => {
 		displayErr.innerText = "it's wrong word."
-		// console.log("Please enter correct word...")
 	});
 
 })
