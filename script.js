64d6f126527d1087eea00edfb93280aaa30a16f6
ErShubham2908 <shubhamprepbytes007@gmail.com>
2023-03-06 15:22:38 +0530

    add css

---
+++ script.js
@@ -0,0 +1,35 @@
+let btn = document.getElementById('search');
+let words = document.getElementById('get_word');
+let phonetic = document.getElementById('phonetic');
+let audio = document.getElementById('audio');
+// let sound = document.getElementById('audioSource');
+let item_partOfSpeech = document.getElementById('item_partOfSpeech');
+
+btn.addEventListener('click', () => {
+	let displayErr = document.getElementById('display');
+	let input = document.getElementById('word').value;
+	input = input.toLowerCase();
+	console.log(input)
+	let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`
+
+	fetch(url)
+	.then((d) => d.json())
+	.then((item) => {
+		item.map((data) => {
+			words.innerText =  `${data.word}`;
+			if(data.phonetic === undefined){
+				phonetic.style.display = 'none';
+			}
+			else{
+				phonetic.innerText = data.phonetic;
+			}
+			item_partOfSpeech.innerText = data.meanings[0].partOfSpeech;
+			item_definition.innerText = data.meanings[0].definitions[0].definition;
+		})
+	})
+	.catch((err) => {
+		displayErr.innerText = "it's wrong word."
+		// console.log("Please enter correct word...")
+	});
+
+})
